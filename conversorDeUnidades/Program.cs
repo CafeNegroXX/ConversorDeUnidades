09e5ebae299a18b0f118b5df1b9c141abb362958
CafeNegroXX <arielsantos772@gmail.com>
2023-11-21 15:46:31 -0300

    first commit

---
+++ conversorDeUnidades/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Unitconverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conversor de longitud en metros");
+
+            double meters = Convert.ToDouble(Console.ReadLine());
+
+            double feet = meters * 3.28084;
+
+            Console.WriteLine("{0} metros son {1} pies ", meters, feet);
+
+
+        }
+    }
+}
